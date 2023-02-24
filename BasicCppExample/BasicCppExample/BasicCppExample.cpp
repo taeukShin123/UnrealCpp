affad5bedebda73362fa8fbab104be12fa87e313
Henry <shintaeuk91@gmail.com>
2023-02-24 13:20:08 +0900

    Basic Cpp part2 - Data Types Variables and Constants

--- BasicCppExample/BasicCppExample/BasicCppExample.cpp
+++ BasicCppExample/BasicCppExample/BasicCppExample.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	/*
 	const int SecondsPerMinutes{ 60 };
 	// int total_seconds_played; // Snake case
 	// int totalSecondsPlayed; // Camel case
@@ -12,4 +13,16 @@ int main()
 	int SecondsPlayed = TotalSecondsPlayed % SecondsPerMinutes;
 	std::cout << "Minuites Played: " << MinutesPlayed << "\n";
 	std::cout << "Seconds Played: " << SecondsPlayed << "\n";
+	*/
+
+	float Angle{ 45 };
+
+	std::cout << "Enter angle in degrees: ";
+	std::cin >> Angle;
+	// convert angle to radians - 360 degree = 2pi radians -> 180 degree = pi radians -> radian = pi / 180
+	const double PI = std::acos(-1);
+	Angle = Angle * PI / 180;
+
+	float Cosine = cosf(Angle);
+	std::cout << "Cosine of " << &Angle << " degrees: " << Cosine << "\n";
 }
